2a1e938c52d2d92f6605dd71e379ccee5fb3f87e
vigorring <vigorring0914@gmail.com>
2023-02-03 16:03:01 +0900

    style: change styles in every elements

--- src/pages/home.js
+++ src/pages/home.js
@@ -91,28 +91,33 @@ export default function TreeViewFile() {
             >
               {({ errors, touched }) => (
                 <Form className="p-30">
-                  <h1 className="text-3xl font-bold mb-10">
+                  <h1 className="text-16 font-medium mb-20">
                     Please enter your name and pick the Sectors you are
                     currently involved in.
                   </h1>
-                  <div className="mb-10 flex items-center">
-                    <p className="text-gray-700 text-sm font-bold">Name :</p>
-                    <Field
-                      className="border border-[#5f5f5f] rounded-md ml-2"
-                      name="username"
-                    />
+                  <div className="mb-10">
+                    <div className="flex items-center">
+                      <p className="text-gray-700 text-sm mr-10">Name :</p>
+                      <Field
+                        className="border border-slate-300 rounded-md ml-2 pl-10  pr-10"
+                        name="username"
+                      />
+                    </div>
                     {touched.username && errors.username && (
-                      <div>{errors.username}</div>
+                      <div className="text-red-500 text-10 ml-60 ">
+                        {errors.username}
+                      </div>
                     )}
                   </div>
                   <div className="sectors mb-10">
                     <label>Sectors : </label>
                     <TreeView
+                      className="border border-slate-300 rounded-md"
                       defaultCollapseIcon={<ExpandMoreIcon />}
                       defaultExpanded={["1"]}
                       defaultExpandIcon={<ChevronRightIcon />}
                       sx={{
-                        height: 240,
+                        height: 400,
                         flexGrow: 1,
                         maxWidth: 600,
                         overflowY: "auto",
@@ -123,11 +128,19 @@ export default function TreeViewFile() {
                     </TreeView>
                     This is the selectedNode: {selectedNode?.title}
                   </div>
-                  <div className="agree mb-10">
-                    <Field type="checkbox" name="toggle" />
-                    <label>Agree to terms</label>
+                  <div className="mb-20">
+                    <div className="agree flex items-center">
+                      <Field
+                        type="checkbox"
+                        name="toggle"
+                        className="w-15 h-15 mr-5"
+                      />
+                      <label>Agree to terms</label>
+                    </div>
                     {touched.toggle && errors.toggle && (
-                      <div>{errors.toggle}</div>
+                      <div className="text-red-500 text-10">
+                        {errors.toggle}
+                      </div>
                     )}
                   </div>
                   <div className="actions">
