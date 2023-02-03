fed4476f45198e88a00127990722f7b01d5021fc
vigorring <vigorring0914@gmail.com>
2023-02-03 15:26:33 +0900

    feat: update mockup data

--- src/mock/mockdata.js
+++ src/mock/mockdata.js
@@ -2,39 +2,56 @@ export const mockupData = [
   {
     id: "1",
     title: "Manufacturing",
-    childNodes: [
-      {
-        id: "2",
-        parentId: "1",
-        title: "Construction materials"
-      },
-      {
-        id: "3",
-        parentId: "1",
-        title: "Electronics and Optics"
-      },
-      {
-        id: "4",
-        parentId: "1",
-        title: "Food and Beverage",
-        childNodes: [
-          {
-            id: "5",
-            parentId: "4",
-            title: "Bakery & confectionery products",
-          },
-          {
-            id: "6",
-            parentId: "4",
-            title: "Beverages",
-          },
-          {
-            id: "7",
-            parentId: "4",
-            title: "Fish & fish products ",
-          },
-        ],
-      },
-    ],
-  }
+    parentId: "0",
+  },
+  {
+    id: "2",
+    title: "Construction materials",
+    parentId: "1",
+  },
+  {
+    id: "3",
+    title: "Electronics and Optics",
+    parentId: "1",
+  },
+  {
+    id: "4",
+    title: "Food and Beverage",
+    parentId: "1",
+  },
+  {
+    id: "5",
+    title: "Bakery & confectionery products",
+    parentId: "4",
+  },
+  {
+    id: "6",
+    title: "Beverages",
+    parentId: "4",
+  },
+  {
+    id: "7",
+    title: "Fish & fish products",
+    parentId: "4",
+  },
+  {
+    id: "8",
+    title: "Meat & meat products",
+    parentId: "4",
+  },
+  {
+    id: "9",
+    title: "Milk & dairy products",
+    parentId: "4",
+  },
+  {
+    id: "10",
+    title: "Other",
+    parentId: "4",
+  },
+  {
+    id: "11",
+    title: "Sweets & snack food",
+    parentId: "4",
+  },
 ];
